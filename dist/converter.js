80c222510297bb14d0daff7c6a0e766e0b60ec55
Sukun-R <111569426+Sukun-R@users.noreply.github.com>
2026-03-22 00:05:24 +0900

    Add files via upload

--- dist/converter.js
+++ dist/converter.js
@@ -1,6 +1,6 @@
 async function sendSentencesData(str){if(inputtype==="roma"){const kanji=str;const hiragana=await KanjiToHiragana(str);const roma=await HiraganaToRoma(hiragana);return{kanji:kanji,hiragana:hiragana,roma:roma,}}
 if(inputtype==="kana"){const kanji=str;const hiragana=await KanjiToHiragana(str);return{kanji:kanji,hiragana:hiragana,}}}
-function createTokenizer(){return new Promise((resolve,reject)=>{kuromoji.builder({dicPath:"./dict/"}).build((err,tokenizer)=>{if(err)reject(err);else resolve(tokenizer)})})}
+function createTokenizer(){return new Promise((resolve,reject)=>{kuromoji.builder({dicPath:"../dict/"}).build((err,tokenizer)=>{if(err)reject(err);else resolve(tokenizer)})})}
 async function KanjiToHiragana(str){try{const tokenizer=await createTokenizer();const tokens=tokenizer.tokenize(str);const hiragana=katakanaToHiragana(tokens.map(t=>t.reading||t.surface_form).join(""));return hiragana}catch{alert("error\nファイル内に不正な文字が含まれている可能性があります。")}}
 function katakanaToHiragana(str){return str.replace(/[\u30a1-\u30f6]/g,ch=>{if(ch==="ヴ")return"ゔ";return String.fromCharCode(ch.charCodeAt(0)-0x60)})}
 async function HiraganaToRoma(str){const roma=[];const sutegana="ぁぃぅぇぉゃゅょ";const forN="あいうえおなにぬねのやゆよん";for(let i=0;i<str.length;i++){if(str[i]==="ん"&&(forN.includes(str[i+1])||str[i+1]===undefined||str[i+1]==="\r")){if(str[i+1]==="う"){roma.push(["nnu","xnu","nnwu","xnwu","nnwhu","xnwhu","nwu","nwhu",]);i++}else{roma.push(["nn","xn"])}}else{if(roma_table_1[str[i]]&&!sutegana.includes(str[i+1])&&!roma_table_3[str[i]+str[i+1]]){roma.push(roma_table_1[str[i]])}
